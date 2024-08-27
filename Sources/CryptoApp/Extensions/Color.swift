2097824a6ad27a156adb484478c2e5857b129f3e
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-27 03:44:18 +0200

    Add start setUp an color theme

---
+++ Sources/CryptoApp/Extensions/Color.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  CryptoApp
+//
+//  Created by Chau Hoang Khoa on 5/09/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let SecondaryText = Color("SecondaryTextColor")
+}
+
+extension Color {
+    
+    /// Color setUp for app
+    static let theme = ColorTheme()
+}
