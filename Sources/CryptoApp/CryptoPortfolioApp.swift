23bec019a978fe040e75140832a5221eead844e1
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-27 03:01:42 +0200

    Start crypto portfolio app

---
+++ Sources/CryptoApp/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Chau Hoang Khoa on 5/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
