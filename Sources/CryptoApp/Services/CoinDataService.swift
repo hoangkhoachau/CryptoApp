d36d5b77033f158cf348531628824ce567fa1215
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-29 06:46:26 +0200

    Add DataCervice and NetworkingManager

---
+++ Sources/CryptoApp/Services/CoinDataService.swift
@@ -0,0 +1,34 @@
+//
+//  CoinDataService.swift
+//  CryptoApp
+//
+//  Created by Chau Hoang Khoa on 7/09/2024.
+//
+
+import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+    /// Single subscriber
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        /// URL CoinGecko
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h&locale=en") else {
+            return
+        }
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion:(NetworkingManager.handleCompletion(_:)), receiveValue: { [weak self] returnValue in
+                self?.allCoins = returnValue
+                self?.coinSubscription?.cancel()
+            })
+    }
+}
