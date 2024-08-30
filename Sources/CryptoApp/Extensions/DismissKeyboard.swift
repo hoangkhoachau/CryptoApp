dbaf936250b3fea12271b1797fe2afdfbe978131
Chau Hoang Khoa <21120481@student.hcmus.edu.vn>
2024-08-31 01:39:24 +0200

    Add TextField SearchBar

---
+++ Sources/CryptoApp/Extensions/DismissKeyboard.swift
@@ -0,0 +1,15 @@
+//
+//  DismissKeyboard.swift
+//  CryptoApp
+//
+//  Created by Chau Hoang Khoa on 9/09/2024.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
